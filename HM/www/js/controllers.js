112a6971dd13325c5ad31d40cc42d2efd91bbf9c
zl_sam <zl_sam@163.com>
2016-04-27 13:41:48 +0800

    1
    1

--- HM/www/js/controllers.js
+++ HM/www/js/controllers.js
@@ -117,6 +117,45 @@ angular.module('starter.controllers', [])
     }])
 
   .controller('ShoppingCtrl', function($scope) {
+    $scope.items = [
+      {name: "最新发布", value: 0},
+      {name: "浏览最多", value: 1},
+      {name: "已解决", value: 2},
+      {name: "未解决", value: 3}
+    ];
+
+
+    //筛选条件弹出框
+    var template = '';
+    $scope.popover = $ionicPopover.fromTemplateUrl('my-popover.html',{
+      scope:$scope
+    }).then(function(popover){
+      $scope.popover = popover;
+    });
+
+    $scope.openPopover = function($event) {
+      $scope.popover.show($event);
+    };
+    $scope.closePopover = function() {
+      $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function() {
+      // Execute action
+    });
+
+    //触发选择事件
+    $scope.clickStatus = function(value){
+      $scope.popover.hide();
+    }
   })
 
   .controller('MsgCtrl', function($scope) {
