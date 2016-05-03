a5b4cf6b7983a01bad366ec556a36db5d0960bba
steavengong <842228597@qq.com>
2016-05-03 09:35:49 +0800

    城市filter选择器

--- HM/www/js/filters/cityFilters.js
+++ HM/www/js/filters/cityFilters.js
@@ -3,15 +3,27 @@ angular.module("city.filters",[])
     return function(input,filter){
       if(filter){
         filter = filter.toLowerCase();
-        //console.log(filter);
-
-        /*var cities = [];
+        var cities = [];
         for(var i=0 ; i<input.length;i++){
           var city = input[i];
-
-
-        }*/
-
+          if(city.cityName.toLowerCase() == filter){
+            cities.push(city);
+            continue;
+          }
+          else{
+            var filters = city.filter?city.filter.split(";"):[];
+            for(var j = 0 ; j<filters.length ; j++){
+              if(filters[j].toLowerCase() == filter){
+                cities.push(city);
+                break;
+              }
+              else{
+                continue;
+              }
+            }
+          }
+        }
+        return cities;
       }
       return input;
     }
